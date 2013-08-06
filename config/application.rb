811faeb3ea1314a6bdecdc0e22004c18a89abdea
loic <loic@cloud66.com>
2013-08-06 13:24:14 +0100

    asset pipeline compilation set to true

--- config/application.rb
+++ config/application.rb
@@ -54,6 +54,7 @@ module SampleRails326Mysql
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
+    config.assit.pipeline = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
